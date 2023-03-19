d89bf6ecc4b553230d2d3f21bc89146c970d6fca
Yuuu <Yuuu@DESKTOP-096DUOQ>
2023-03-19 18:22:05 +0800

    加入專案檔案。

---
+++ ITSA題目11/ITSA題目11.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int R, C;
+    int a[100][100];
+    while (cin >> R >> C) {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                cin >> a[i][j];
+            }
+        }
+
+        for (int i = 0; i < C; i++) {
+            for (int j = 0; j < R; j++) {
+                cout << a[j][i] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
